dde5287319ce60ce8cf4860ec03a44a716c48dcd
sivanotc <sivanotc@CLASS42>
2018-10-16 19:40:57 +0300

    "Class update"

--- OpencartAPITest/GlobalData.cs
+++ OpencartAPITest/GlobalData.cs
@@ -8,6 +8,41 @@ namespace OpencartAPITest
 {
     class GlobalData
     {
+        string username;
+        string apitoken;
+        string URL;
+
+        public GlobalData() { }
+
+        public string Getusername() {
+            return this.username;
+        }
+
+        public string Getapitoken()
+        {
+            return this.apitoken;
+        }
+
+        public string GetURL()
+        {
+            return this.URL;
+        }
+
+        public void Setusername(string username)
+        {
+            this.username = username;
+        }
+
+        public string Setapitoken()
+        {
+            return this.apitoken;
+        }
+
+        public string SetURL()
+        {
+            return this.URL;
+        }
+
 
 
     }
