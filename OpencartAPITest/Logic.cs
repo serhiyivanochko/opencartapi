bd47797dc316f3ff991f76147568800a8870b772
sivanotc <sivanotc@CLASS42>
2018-10-19 18:39:02 +0300

    "Added Login method"

---
+++ OpencartAPITest/Logic.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestSharp;
+using Newtonsoft.Json;
+
+namespace OpencartAPITest
+{
+    public class LoginMessage {
+
+        [JsonProperty("success")]
+        public string status { get; set; }
+        [JsonProperty("api_token")]
+        public string api_token { get; set; }
+    }
+
+    public class Logic
+    {
+        public string GetApiToken() {
+            var client = new RestClient(GlobalData.URL + "login");
+
+            var request = new RestRequest(Method.POST);
+            request.AddParameter("username", GlobalData.username);
+            request.AddParameter("key", GlobalData.key);
+
+            var response = client.Execute(request);
+            
+            LoginMessage login = JsonConvert.DeserializeObject<LoginMessage>(response.Content);
+            return login.api_token;
+        }
+    }
+}
--- OpencartAPITest/UnitTest1.cs
+++ OpencartAPITest/UnitTest1.cs
@@ -11,13 +11,8 @@ namespace OpencartAPITest
         [Test]
         public void TestLogin()
         {
-            var client = new RestClient(GlobalData.URL+"login");
-
-            var request = new RestRequest(Method.POST);
-            request.AddParameter("username", GlobalData.username);
-            request.AddParameter("key", GlobalData.key);
-
-            var response = client.Execute(request);
+            Logic logic = new Logic();
+            logic.GetApiToken();
 
             
 
