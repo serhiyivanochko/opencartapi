a4fe6ed20a95805148b39118d42d678a523a77d1
Second <second@mail.com>
2018-10-19 15:36:13 +0300

    Update

--- OpencartAPITest/GlobalData.cs
+++ OpencartAPITest/GlobalData.cs
@@ -6,42 +6,43 @@ using System.Threading.Tasks;
 
 namespace OpencartAPITest
 {
-    class GlobalData
+    public static class GlobalData
     {
-        string username;
-        string apitoken;
-        string URL;
+        public static string apitoken;
+        public static string URL = "http://40.118.125.245/index.php?route=api/";
+        public static string key = "d5YFz2RyNjnNXpkTqpNaoGAIPHuipKbmKnlRwOP2Jrls05gZJi3hDNbS8Orvbm5XAYJZ1ckrL3SQqikPo1V7FyPPiG7JEfYhWqjLHhjvXb0HED3EyNt2CHSVLzNIlgpzWzjXFh2HiHfCJd2XSubGlCTczDR5uXP2V5rNX1Gjt8uK05Hd1eeRiytEmoIEDjeXW1mw14oL1qxSBATmmv5CZJzmSTayghm2cXWZYw1msbPEhuItfrBzXJcuaV188neq";
+        public static string username = "Default";
 
-        public GlobalData() { }
+        //public GlobalData() { }
 
-        public string Getusername() {
-            return this.username;
-        }
+        //public string Getusername() {
+        //    return this.username;
+        //}
 
-        public string Getapitoken()
-        {
-            return this.apitoken;
-        }
+        //public string Getapitoken()
+        //{
+        //    return this.apitoken;
+        //}
 
-        public string GetURL()
-        {
-            return this.URL;
-        }
+        //public string GetURL()
+        //{
+        //    return this.URL;
+        //}
 
-        public void Setusername(string username)
-        {
-            this.username = username;
-        }
+        //public void Setusername(string username)
+        //{
+        //    this.username = username;
+        //}
 
-        public string Setapitoken()
-        {
-            return this.apitoken;
-        }
+        //public string Setapitoken()
+        //{
+        //    return this.apitoken;
+        //}
 
-        public string SetURL()
-        {
-            return this.URL;
-        }
+        //public string SetURL()
+        //{
+        //    return this.URL;
+        //}
 
 
 
--- OpencartAPITest/UnitTest1.cs
+++ OpencartAPITest/UnitTest1.cs
@@ -1,14 +1,26 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RestSharp;
+using NUnit.Framework;
 
 namespace OpencartAPITest
 {
     [TestClass]
-    public class UnitTest1
+    public class OpenCartAPI
     {
-        [TestMethod]
-        public void TestMethod1()
+        [Test]
+        public void TestLogin()
         {
+            var client = new RestClient(GlobalData.URL+"login");
+
+            var request = new RestRequest(Method.POST);
+            request.AddParameter("username", GlobalData.username);
+            request.AddParameter("key", GlobalData.key);
+
+            var response = client.Execute(request);
+
+            
+
         }
     }
 }
